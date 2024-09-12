1777b671881f68e9ab1d48aa6b3bc11a76ba8f2f
Juan David Castelblanco <84249113+jdcastel@users.noreply.github.com>
2024-09-12 15:09:22 -0400

    fix about paragraph

--- app/sections/about/index.jsx
+++ app/sections/about/index.jsx
@@ -24,8 +24,7 @@ export function AboutSection() {
 						}}
 					>
 						<p>
-						Highly motivated software developer, graduated from an advanced diploma in Computer Analysis and Programming at Seneca Polytechnic. Equipped with comprehensive web programming skills gained through rigorous IT training, he also developed a solid foundation in databases and cloud computing, positioning me for success in the dynamic and rapidly evolving field of software development.
-						</p>
+						Highly motivated software developer graduated from an advanced diploma in Computer Analysis and Programming at Seneca Polytechnic. Equipped with comprehensive web programming skills gained through rigorous IT training, also developing a solid foundation in databases and cloud computing, positioning myself for success in the dynamic and rapidly evolving field of software development.</p>
 						<br />
 						<p>
 						I am constantly looking for ways to work efficiently and accurately. By managing many stressful situations and resolving them under time pressure, I have honed my problem-solving skills and learned to prioritize tasks effectively. With a passion to learn and a drive to succeed, I am eager to take on new challenges and contribute to the success of any team.
