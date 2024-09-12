3861007dd6068ddde61241d43baa227910da8aa7
Juan David Castelblanco <84249113+jdcastel@users.noreply.github.com>
2024-09-11 22:18:06 -0400

    minor change

--- app/sections/experience/index.jsx
+++ app/sections/experience/index.jsx
@@ -20,7 +20,7 @@ export function ExperienceSection() {
 					>
 						<h3 className="vertical-timeline-element-title text-black">Low Code Application Developer</h3>
 						<h4 className="vertical-timeline-element-subtitle text-black">Ontario Government </h4>
-						<h3 className="text-light">Developed a web-based project management application using the MERN stack that allowed clients to view the progress of pending tasks.<br/>Led a project team providing guidance and direction facilitating communication and collaboration to ensure timely and successful completion of the project.<br/>Collaborated with an agile team of developers and project managers to create a customized PowerApps application for over 1000 users.<br/>Contributed to the design and implementation of key features, such as data integration with existing systems and user-friendly interface design.<br/>Identified and resolved software bugs through rigorous testing and debugging.<br/>Improved the operational speed of applications by optimizing code and streamlining processes.</h3>
+						<p className="text-light">Developed a web-based project management application using the MERN stack that allowed clients to view the progress of pending tasks.<br/>Led a project team providing guidance and direction facilitating communication and collaboration to ensure timely and successful completion of the project.<br/>Collaborated with an agile team of developers and project managers to create a customized PowerApps application for over 1000 users.<br/>Contributed to the design and implementation of key features, such as data integration with existing systems and user-friendly interface design.<br/>Identified and resolved software bugs through rigorous testing and debugging.<br/>Improved the operational speed of applications by optimizing code and streamlining processes.</p>
 					</VerticalTimelineElement>
 					<VerticalTimelineElement
 						className="vertical-timeline-element--work text-black"
@@ -30,7 +30,7 @@ export function ExperienceSection() {
 					>
 						<h3 className="vertical-timeline-element-title text-black">Technical Support Specialist Volunteer</h3>
 						<h4 className="vertical-timeline-element-subtitle text-black">hispanotech.ca </h4>
-						<h3  className="text-light">Worked closely with other technical support specialists to troubleshoot and resolve complex software issues.<br/>Provided clear and concise instructions for users to navigate the software features and functions, resulting in increased user adoption and satisfaction.<br/>Developed comprehensive process documentation to increase team efficiency and facilitate future troubleshooting.<br/>Demonstrated technical proficiency and problem-solving skills in resolving complex software issues.</h3>
+						<p className="text-light">Worked closely with other technical support specialists to troubleshoot and resolve complex software issues.<br/>Provided clear and concise instructions for users to navigate the software features and functions, resulting in increased user adoption and satisfaction.<br/>Developed comprehensive process documentation to increase team efficiency and facilitate future troubleshooting.<br/>Demonstrated technical proficiency and problem-solving skills in resolving complex software issues.</p>
 					</VerticalTimelineElement>
 				</VerticalTimeline>
 			</section>
