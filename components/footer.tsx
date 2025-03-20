7b03510ab42320dac189e1f4f186365f3dbeb8d0
Juan David Castelblanco <84249113+jdcastel@users.noreply.github.com>
2025-03-20 17:11:35 -0500

    update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,12 +4,13 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2025 Juan Castelblanco. All rights reserved.
+        &copy; 2025 Juan Castelblanco. All rights reserved. This project is a fork of the original project by ByteGrad.
       </small>
       <p className="text-xs">
-        <span className="font-semibold">About this website:</span> built with
-        React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
-        Framer Motion, React Email & Resend, Vercel hosting.
+        <span className="font-semibold">About this website:</span> built with React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS, Framer Motion, Vercel hosting.
+      </p>
+      <p className="text-xs mt-2">
+        Made with love <span role="img" aria-label="green heart">💚</span>
       </p>
     </footer>
   );
